a9ab8f554f5930f26672a719f0b0347a7441e0a0
debrx <amadeborah11@gmail.com>
2024-04-22 02:43:38 +0100

    testing javascript

--- script.js
+++ script.js
@@ -1,66 +1,68 @@
+
+function toggleSection(sectionClass) {
+    var sections = document.getElementsByClassName(sectionClass);
+    for (var i = 0; i < sections.length; i++) {
+        var section = sections[i];
+        section.classList.toggle('hidden');
+    }
+}
+
 document.getElementById('learn').addEventListener('click', function() {
     document.querySelector('.restaurant').scrollIntoView({
         behavior: 'smooth'
     });
-    hidesection('.owner');
-    hidesection('.menu');
+    toggleSection('.owner');
+    toggleSection('.menu');
 });
 
 document.getElementById('ownerbtn').addEventListener('click', function() {
     document.querySelector('.owner').scrollIntoView({
         behavior: 'smooth'
     });
-    hidesection('.restaurant');
-    hidesection('.menu');
+    toggleSection('.restaurant');
+    toggleSection('.menu');
 });
 
 document.getElementById('menucl').addEventListener('click', function() {
     document.querySelector('.menu').scrollIntoView({
         behavior: 'smooth'
     });
-    hidesection('.restaurant');
-    hidesection('.owner');
+    toggleSection('.restaurant');
+    toggleSection('.owner');
 });
 
-function ownersec() {
-    var owners = document.getElementsByClassName("owner");
-    for (var i = 0; i < owners.length; i++) {
-        var owner = owners[i];
-        if (owner.style.display === "none" || owner.style.display === "") {
-            owner.style.display = "flex";
-        } else {
-            owner.style.display = "none";
-        }
-    }
-}
-
-function ressec() {
-    var owners = document.getElementsByClassName("restaurant");
-    for (var i = 0; i < owners.length; i++) {
-        var owner = owners[i];
-        if (owner.style.display === "none" || owner.style.display === "") {
-            owner.style.display = "flex";
-        } else {
-            owner.style.display = "none";
-        }
-    }
-}
-
-function menusec() {
-    var owners = document.getElementsByClassName("menu");
-    for (var i = 0; i < owners.length; i++) {
-        var owner = owners[i];
-        if (owner.style.display === "none" || owner.style.display === "") {
-            owner.style.display = "flex";
-        } else {
-            owner.style.display = "none";
-        }
-    }
-}
-
-function hidesection(sectionClass) {
-    var sections = document.querySelectorAll(sectionClass);
-    for (var i = 0; i < sections.length; i++) {
-        sections[i].style.display = "none";
-    }
-}
+// function ownersec() {
+//     var owners = document.getElementsByClassName("owner");
+//     for (var i = 0; i < owners.length; i++) {
+//         var owner = owners[i];
+//         if (owner.style.display === "none" || owner.style.display === "") {
+//             owner.style.display = "flex";
+//         } else {
+//             owner.style.display = "none";
+//         }
+//     }
+// }
+//
+// function ressec() {
+//     var owners = document.getElementsByClassName("restaurant");
+//     for (var i = 0; i < owners.length; i++) {
+//         var owner = owners[i];
+//         if (owner.style.display === "none" || owner.style.display === "") {
+//             owner.style.display = "flex";
+//         } else {
+//             owner.style.display = "none";
+//         }
+//     }
+// }
+//
+// function menusec() {
+//     var owners = document.getElementsByClassName("menu");
+//     for (var i = 0; i < owners.length; i++) {
+//         var owner = owners[i];
+//         if (owner.style.display === "none" || owner.style.display === "") {
+//             owner.style.display = "flex";
+//         } else {
+//             owner.style.display = "none";
+//         }
+//     }
+// }
