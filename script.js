26e626921fac75482f679a5b440e9e42f40b6a05
debrx <amadeborah11@gmail.com>
2024-04-22 03:09:01 +0100

    testing javascript as external

--- script.js
+++ script.js
@@ -30,30 +30,6 @@ document.addEventListener('DOMContentLoaded', function() {
         hidesection('.owner');
     });
 });
-//
-// document.getElementById('learn').addEventListener('click', function() {
-//     document.querySelector('.restaurant').scrollIntoView({
-//         behavior: 'smooth'
-//     });
-//     hidesection('.owner');
-//     hidesection('.menu');
-// });
-//
-// document.getElementById('ownerbtn').addEventListener('click', function() {
-//     document.querySelector('.owner').scrollIntoView({
-//         behavior: 'smooth'
-//     });
-//     hidesection('.restaurant');
-//     hidesection('.menu');
-// });
-//
-// document.getElementById('menucl').addEventListener('click', function() {
-//     document.querySelector('.menu').scrollIntoView({
-//         behavior: 'smooth'
-//     });
-//     hidesection('.restaurant');
-//     hidesection('.owner');
-// });
 
 function ownersec() {
     var owners = document.getElementsByClassName("owner");
@@ -79,21 +55,78 @@ function ressec() {
     }
 }
 
-// function menusec() {
-//     var owners = document.getElementsByClassName("menu");
-//     for (var i = 0; i < owners.length; i++) {
-//         var owner = owners[i];
-//         if (owner.style.display === "none" || owner.style.display === "") {
-//             owner.style.display = "flex";
-//         } else {
-//             owner.style.display = "none";
-//         }
-//     }
-// }
-
 function menusec() {
     var menus = document.getElementsByClassName("menu");
     Array.from(menus).forEach(function(menu) {
         menu.style.display = (menu.style.display === "none" || menu.style.display === "") ? "flex" : "none";
     });
 }
+
+// Slideshow javascript.
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
+
+
+function backToTop(sectionId) {
+    var section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({behavior: "smooth"});
+    }
+
+}
+
+//Gallery
+
+const indicators = document.querySelectorAll('a.indicator');
+const resetCurrentActiveIndicator = () => {
+    const activeIndicator = document.querySelector(".active");
+    activeIndicator.classList.remove("active");
+};
+indicators.forEach((indicator) => {
+    indicator.addEventListener('click', function () {
+        resetCurrentActiveIndicator();
+        this.classList.add('active');
+    });
+});
+
+const sections = document.querySelectorAll("section");
+
+const onSectionLeavesViewport = (section) => {
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    resetCurrentActiveIndicator();
+                    const element = entry.target;
+                    const indicator = document.querySelector(`a[href='#${element.id}']`);
+                    indicator.classList.add("active");
+                    return;
+                }
+            });
+        },
+        {
+            root: null,
+            rootMargin: "0px",
+            threshold: 0.75
+        }
+    );
+    observer.observe(section);
+};
+
+sections.forEach(onSectionLeavesViewport);
